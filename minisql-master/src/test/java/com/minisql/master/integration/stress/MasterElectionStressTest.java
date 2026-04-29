c539ecbad85e3a4a128b0260b4ca8d57ee69f53f
Jatrawery <2539066538@@qq.com>
2026-04-29 19:37:28 +0800

    fix(test): move misplaced test files to correct location

---
+++ minisql-master/src/test/java/com/minisql/master/integration/stress/MasterElectionStressTest.java
@@ -0,0 +1,30 @@
+package com.minisql.master.integration.stress;
+
+import org.junit.*;
+import org.testcontainers.containers.GenericContainer;
+import static org.junit.Assert.*;
+
+public class MasterElectionStressTest {
+    private static GenericContainer<?> zookeeper;
+
+    @BeforeClass
+    public static void setUpClass() {
+        zookeeper = new GenericContainer<>("zookeeper:3.9.1")
+                .withExposedPorts(2181);
+        zookeeper.start();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        if (zookeeper != null) {
+            zookeeper.stop();
+        }
+    }
+
+    @Test
+    public void testRapidMasterFailover() {
+        // Placeholder - will implement rapid failover stress test
+        String zkConnect = zookeeper.getHost() + ":" + zookeeper.getMappedPort(2181);
+        assertNotNull(zkConnect);
+    }
+}
